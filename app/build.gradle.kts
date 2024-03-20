efbcb1b512a2083664faf5696efa4b68d38fc865
Ali Kazal <ali.kazal11@gmail.com>
2024-03-20 10:12:17 -0600

    1.0.0-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,5 +65,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation ("com.github.TalkShopLive:android-sdk:0.1.8-alpha")
+    implementation ("com.github.TalkShopLive:android-sdk:1.0.0-beta")
 }
